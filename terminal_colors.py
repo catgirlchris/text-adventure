91d523eef3793f01d081f0f26407b258c9163c08
catgirlchris <chrisfergo99@gmail.com>
2023-05-31 16:38:56 +0200

    first try at mouse integration

--- terminal_colors.py
+++ terminal_colors.py
@@ -14,6 +14,7 @@ def main(stdscr:'curses._CursesWindow'):
         curses.init_pair(CP_BLANK, 0, 0)
 
     curses.curs_set(0)
+    curses.mousemask(-1)
 
     stdscr.insstr(0,0, f'COLORS: {curses.COLORS}')
     stdscr.insstr(1,0, f'COLOR PAIRS: {curses.COLOR_PAIRS}, DEFAULT_PAIR: {curses.color_content(0)}')
@@ -43,6 +44,8 @@ def main(stdscr:'curses._CursesWindow'):
     color_index = 0
 
     def draw_colors(colorpad:'curses._CursesWindow', color_structure:list[int]):
+        # first line
+        colorpad.addstr('\n', curses.color_pair(0))
         color = 0
         for e in color_structure:
             for i in range(0,e,1):
@@ -56,7 +59,7 @@ def main(stdscr:'curses._CursesWindow'):
 
     # surrounding rectangle draw
     curses.textpad.rectangle(stdscr, beg[0]-2,beg[1]-2, beg[0]+size[0]+3, beg[1]+size[1]+1)
-    #colorpad.border()
+
     # title
     stdscr.addstr(beg[0]-2,beg[1]+3,
                   'COLORCILLOS DISPONIBLES', curses.color_pair(0))
@@ -67,71 +70,74 @@ def main(stdscr:'curses._CursesWindow'):
     # debug pause
     stdscr.getch()
 
-    #curses.textpad.rectangle(colorpad, beg[0],beg[1]-2, beg[0]+2,beg[1]+2)
-    colorpad_refresh()
-
-    draw_highlight_rect = lambda: curses.textpad.rectangle(
-                                    colorpad,
-                                    beg[0]+offset*2,beg[1]-3,
-                                    beg[0]+2+offset*2,beg[1]+1)
-
-    erase_highlight_rect = lambda: curses.textpad.rectangle(
-                                    colorpad,
-                                    beg[0]+offset*2-2,beg[1]-3,
-                                    beg[0]+2+offset*2-2,beg[1]+1)
-
-    highlightpad = curses.newpad(3,10)
+    hlpad_pos = [3,0]
+    hlpad_size = [3,10]
+    highlightpad = curses.newpad(hlpad_size[0],hlpad_size[1])
     highlightpad.border()
-    highlightpad.overlay(colorpad, 0,0, 3,0, 5,9)
+    hl_overlay = lambda: highlightpad.overlay(colorpad, 0,0, 
+                         hlpad_pos[0],hlpad_pos[1], 
+                         hlpad_pos[0]+hlpad_size[0]-1,hlpad_pos[1]+hlpad_size[1]-1)
+
+    hl_refresh = lambda: highlightpad.refresh(0,0, 
+                         hlpad_pos[0],hlpad_pos[1], 
+                         hlpad_pos[0]+hlpad_size[0]-1,hlpad_pos[1]+hlpad_size[1]-1)
+
+    '''hl_overlay()
     colorpad_refresh()
     stdscr.getch()
-    #highlightpad.clear()
+
     highlightpad.erase()
+    colorpad.erase()
+    draw_colors(colorpad, color_structure)
     colorpad_refresh()
     stdscr.getch()
-    #colorpad.erase()
-    #draw_colors(colorpad, color_structure)
-    #colorpad.border()
-    #colorpad_refresh()
-    #stdscr.getch()
+
+    stdscr.getch()
+    '''
+
 
     colorpad.scrollok(True)
     end = True
+
     # Scrolling loop
     while(end):
-        # erase last rectangle
-        #colorpad.attron(curses.color_pair(0))
-        #erase_highlight_rect()
-        #colorpad.attroff(curses.color_pair(0))
-
-        colorpad.attron(curses.color_pair(1))
-        #draw_highlight_rect()
-        colorpad.attroff(curses.color_pair(1))
-
-        colorpad_refresh()
-
-        key :str = stdscr.getch()
-        if (key == curses.KEY_DOWN):
-            #colorpad.scroll(1)
-            offset = min(offset+1, len(color_structure)+size[0])
-        elif (key == curses.KEY_UP):
-            #colorpad.scroll(-1)
-            offset = max(offset-1, 0)
-        elif (key == curses.KEY_RIGHT):
+        event :str = stdscr.getch()
+        if (event == curses.KEY_DOWN):
+            colorpad.erase()
+            hlpad_pos[0] = min(hlpad_pos[0]+2, len(color_structure)+size[0]*2)
+            draw_colors(colorpad, color_structure)
+            highlightpad.border()
+            hl_overlay()
+            offset = min(offset+2, len(color_structure)+size[0])
+        elif (event == curses.KEY_UP):
+            colorpad.erase()
+            hlpad_pos[0] = max(hlpad_pos[0]-2, 0)
+            draw_colors(colorpad, color_structure)
+            highlightpad.border()
+            hl_overlay()
+            offset = max(offset-2, 0)
+        elif (event == curses.KEY_RIGHT):
             color_structure[offset] += 1
             color_structure[offset+1] -= 1
             draw_colors(colorpad, color_structure)
-        elif (key == curses.KEY_LEFT):
+        elif (event == curses.KEY_LEFT):
             color_structure[offset] -= 1
             color_structure[offset-1] += 1
             draw_colors(colorpad, color_structure)
 
+        if event == curses.KEY_MOUSE:
+            _, mx, my, _, bstate = curses.getmouse()
+            y, x = my - beg[0] + offset, mx-3
+            if (bstate == curses.BUTTON1_CLICKED, colorpad.enclose(y, x)):
+                #stdscr.addstr(y, x, stdscr.instr(my, mx, 5))
+                hlpad_pos[0],hlpad_pos[1] = y, x
+                draw_colors(colorpad, color_structure)
+                highlightpad.border()
+                hl_overlay()
+
         # KEY TO EXIT
-        elif (key == curses.KEY_ENTER):
+        elif ((event == curses.KEY_ENTER) | (event == ord('q'))):
             end = False
-        
-        # draw new rectangle
-        #curses.textpad.rectangle(colorpad, beg[0]+offset*2,beg[1]-2, beg[0]+2+offset*2,beg[1]+2)
 
         # draw colorpad
         colorpad_refresh()
